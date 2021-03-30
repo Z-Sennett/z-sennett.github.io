1cff6ed867b43bd49f1afb2311478739728b019c
Z-Sennett <77120073+Z-Sennett@users.noreply.github.com>
2021-03-30 10:16:29 -0400

    Captain America Form

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,39 @@ $(document).ready(function(){
     
     });
 });
+
+var cap = {};
+  cap['steve'] = 0;
+  cap['bucky'] = 0;
+$(document).ready(function(){
+  
+  drawChart();
+  
+  $("#submit").click(function(){
+      if($("#bettercap").val()=="steve"){
+        cap['steve']++;
+      }
+      if($("#bettercap").val()=="bucky"){
+          cap['bucky']++;
+       }
+    drawChart();
+  });
+  
+  function drawChart(){
+    var ctx = $('#myChart');
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Steve Rogers', 'Bucky Barnes'],
+            datasets: [{
+                label: '# of Votes',
+                data: [cap['steve'], cap['bucky']],
+              backgroundColor:[
+                'tan',
+                'red'
+              ]
+            }]
+            }
+        });
+  }
+  });
